3482c10a0be434c0ae79d77ee9b69668baaea331
Aaronmine <troublor9816@gmail.com>
2021-03-01 16:53:57 +0800

    :sparkle: assignment

---
+++ src/main/java/comp5111/assignment/Assignment2.java
@@ -0,0 +1,8 @@
+package comp5111.assignment;
+
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        // TODO we decide to not restrict how you implement main class. So do as you wish to complete assignment 2.
+    }
+}
